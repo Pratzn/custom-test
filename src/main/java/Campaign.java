977f3fe9c84558ab0c8181c59cdb5cd7c4c4a9b3
Prat Nudklin <pratz.nud@gmail.com>
2025-06-03 22:39:37 +0700

    initial

---
+++ src/main/java/Campaign.java
@@ -0,0 +1,12 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Campaign {
+
+        String name;
+        String type;
+        //Rule rule;
+
+}
